a7aa608992f6d2f84e88d4af820bd64f2979a89e
JeffersonAguiar <jfdruida@gmail.com>
2024-06-03 10:39:01 +0100

    chore: Add playerMoves and choices properties to game object

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -19,4 +19,13 @@ describe("game object contains the correct keys", () => {
   test("currentGame key exists", () => {
     expect("currentGame" in game).toBe(true);
   });
+  test("playerMoves key exists", () => {
+    expect("playerMoves" in game).toBe(true);
+  });
+  test("choices key exists", () => {
+    expect("choices" in game).toBe(true);
+  });
+  test("choices key has the correct values", () => {
+    expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+  });
 });
