b59143224f8b90a306c004c87795f0cdd1ec45eb
JeffersonAguiar <jfdruida@gmail.com>
2024-06-03 10:33:51 +0100

    chore: Update package.json with new Jest dependency and configuration for game.test.js

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,22 @@
 /**
  * @jest-environment jsdom
  */
+
+const { game } = require("../game.js");
+
+beforeAll(() => {
+  let fs = require("fs");
+  let fileContents = fs.readFileSync("index.html", "utf-8");
+  document.open();
+  document.write(fileContents);
+  document.close();
+});
+
+describe("game object contains the correct keys", () => {
+  test("score key exists", () => {
+    expect("score" in game).toBe(true);
+  });
+  test("currentGame key exists", () => {
+    expect("currentGame" in game).toBe(true);
+  });
+});
