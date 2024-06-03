e61117a083c12dbb5456737717790558335507b7
JeffersonAguiar <jfdruida@gmail.com>
2024-06-03 11:46:12 +0100

    Refactor game.js and tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
 
 beforeAll(() => {
   let fs = require("fs");
@@ -12,7 +12,7 @@ beforeAll(() => {
   document.close();
 });
 
-describe("game object contains the correct keys", () => {
+describe("game object contains correct keys", () => {
   test("score key exists", () => {
     expect("score" in game).toBe(true);
   });
@@ -25,7 +25,7 @@ describe("game object contains the correct keys", () => {
   test("choices key exists", () => {
     expect("choices" in game).toBe(true);
   });
-  test("choices key has the correct values", () => {
+  test("choices contain correct ids", () => {
     expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
   });
 });
@@ -47,7 +47,30 @@ describe("newGame works correctly", () => {
   test("should clear the player moves array", () => {
     expect(game.playerMoves.length).toBe(0);
   });
-  test("should clear the computer sequence array", () => {
-    expect(game.currentGame.length).toBe(0);
+  test("should add one move to the computer's game array", () => {
+    expect(game.currentGame.length).toBe(1);
+  });
+});
+
+describe("gameplay works correctly", () => {
+  beforeEach(() => {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    addTurn();
+  });
+  afterEach(() => {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+  });
+  test("addTurn adds a new turn to the game", () => {
+    addTurn();
+    expect(game.currentGame.length).toBe(2);
+  });
+  test("should add correct class to light up the buttons", () => {
+    let button = document.getElementById(game.currentGame[0]);
+    lightsOn(game.currentGame[0]);
+    expect(button.classList).toContain(game.currentGame[0] + "light");
   });
 });
