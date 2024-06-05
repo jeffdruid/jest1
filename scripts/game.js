c43cabd511da5d772b5d0fbcba44be5a040e5673
JeffersonAguiar <jfdruida@gmail.com>
2024-06-05 19:08:27 +0100

    chore: Update game.js and tests with showTurns functionality

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,8 @@ let game = {
   playerMoves: [],
   score: 0,
   turnNumber: 0,
+  lastButton: "",
+  turnInProgress: false,
   choices: ["button1", "button2", "button3", "button4"],
 };
 
@@ -10,6 +12,21 @@ function newGame() {
   game.currentGame = [];
   game.playerMoves = [];
   game.score = 0;
+
+  for (let circle of document.getElementsByClassName("circle")) {
+    if (circle.getAttribute("data-listener") !== "true") {
+      circle.addEventListener("click", (e) => {
+        if (game.currentGame.length > 0 && !game.turnInProgress) {
+          let move = e.target.getAttribute("id");
+          game.lastButton = move;
+          game.playerMoves.push(move);
+          lightsOn(move);
+          playerTurn();
+        }
+      });
+      circle.setAttribute("data-listener", "true");
+    }
+  }
   showScore();
   addTurn();
 }
@@ -21,12 +38,14 @@ function addTurn() {
 }
 
 function showTurns() {
+  game.turnInProgress = true;
   game.turnNumber = 0;
   let turns = setInterval(function () {
     lightsOn(game.currentGame[game.turnNumber]);
     game.turnNumber++;
     if (game.turnNumber >= game.currentGame.length) {
       clearInterval(turns);
+      game.turnInProgress = false;
     }
   }, 800);
 }
@@ -38,8 +57,30 @@ function lightsOn(circ) {
   }, 400);
 }
 
+function playerTurn() {
+  let i = game.playerMoves.length - 1;
+  if (game.currentGame[i] === game.playerMoves[i]) {
+    if (game.currentGame.length === game.playerMoves.length) {
+      game.score++;
+      showScore();
+      addTurn();
+    }
+  } else {
+    alert("Wrong move!");
+    newGame();
+  }
+}
+
 function showScore() {
   document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
+module.exports = {
+  game,
+  newGame,
+  showScore,
+  addTurn,
+  lightsOn,
+  showTurns,
+  playerTurn,
+};
